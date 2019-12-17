9b76aa0c7756ac2a16344fb702da2fbfebb5fa13
mincatsg <1374723981@qq.com>
2019-12-17 20:54:34 +0800

    test

---
+++ ClassObject.java
@@ -0,0 +1,11 @@
+package lessson2;
+
+public class ClassObject {
+
+    public static void main(String[] args) {
+        //类也是java中的对象
+        Class<ClassObject> classObjectClass = ClassObject.class;
+        ClassObject object = new ClassObject();
+        Class classobjectClass2  = object.getClass();
+    }
+}
