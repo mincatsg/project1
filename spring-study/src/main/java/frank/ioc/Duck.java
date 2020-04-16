504281fdd845e2d6bac3551d477246c5fd977b03
mincatsg <1374723981@qq.com>
2020-04-16 21:43:15 +0800

    Spring练习

---
+++ spring-study/src/main/java/frank/ioc/Duck.java
@@ -0,0 +1,36 @@
+package frank.ioc;
+
+public class Duck {
+
+    private String name;
+
+    private Integer age;
+
+    public Duck(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Duck{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
