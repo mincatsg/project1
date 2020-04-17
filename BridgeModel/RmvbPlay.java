0aa27a7b266553d77de7b58be7bf2cd152ac30e1
mincatsg <1374723981@qq.com>
2020-04-18 00:01:40 +0800

    桥接模式

---
+++ BridgeModel/RmvbPlay.java
@@ -0,0 +1,8 @@
+package BridgeModel;
+
+class RmvbPlay implements MediaPlayInter {  //定义RMVB播放器接口
+    public void mediaPlay() {
+        System.out.print("Rmvb格式视频");  //调用成功输出结果
+        System.out.println("");
+    }
+}
