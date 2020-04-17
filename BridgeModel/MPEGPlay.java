907c1233eb05c72ff37525387094f6cf34948927
mincatsg <1374723981@qq.com>
2020-04-17 23:58:37 +0800

    桥接模式

---
+++ BridgeModel/MPEGPlay.java
@@ -0,0 +1,9 @@
+package BridgeModel;
+
+class MPEGPlay implements  MediaPlayInter {  //定义MPEG播放器接口
+    public void mediaPlay() {
+        System.out.print("MPEG格式视频");   //调用成功输出结果
+        System.out.println("");
+    }
+}
+
---
+++ BridgeModel/MediaPlayInter.java
@@ -0,0 +1,8 @@
+package BridgeModel;
+
+interface MediaPlayInter {  //播放器接口
+
+    void mediaPlay();
+
+}
+
