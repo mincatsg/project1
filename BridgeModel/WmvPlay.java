b8759f5d9e367209a2850282fab8221f1182db5b
mincatsg <1374723981@qq.com>
2020-04-18 00:03:48 +0800

    桥接模式

---
+++ BridgeModel/WmvPlay.java
@@ -0,0 +1,8 @@
+package BridgeModel;
+
+class WmvPlay implements MediaPlayInter {  //定义WMV播放器接口
+    public void mediaPlay() {
+        System.out.print("Wmv格式视频");//调用成功输出结果
+        System.out.println("");
+    }
+}
