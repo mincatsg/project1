9fcc252e6e96368b69f8c1129c02c557211ca94c
mincatsg <1374723981@qq.com>
2020-04-29 11:15:30 +0800

    多线程

---
+++ Mythread/src/lesson2/UnsafeTest.java
@@ -0,0 +1,30 @@
+package lesson2;
+
+public class UnsafeTest {
+    private static int COUNT;
+    //1.初始化一个变量值 = 0;
+    //2.启动20个线程，每个线程对这个值++,
+    //执行10000次
+
+    public static void main(String[] args) {
+   Object o = new Object();
+        for(int i = 0; i < 20; i++){
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for(int j = 0; j < 10000; j++) {
+                        synchronized (o) {
+                            COUNT++;
+                        }
+                    }
+                }
+            }).start();
+        }
+
+        // >1 使用debug方式启动
+        while (Thread.activeCount() > 1){
+            Thread.yield(); //线程让步,从运行态转变为就绪态.
+        }
+        System.out.println(COUNT);
+    }
+}
