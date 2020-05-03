e285b14b4508b04339d66c21bbb5a1f6a72ce9d8
mincatsg <1374723981@qq.com>
2020-05-03 18:57:39 +0800

    多线程

---
+++ Mythread/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double n = Double.MAX_VALUE;
+        double n1 = 1.5;
+        double res = Math.max(n, n1);
+        System.out.println(res);
+
+        System.out.println(Math.abs(-1 - 3));
+    }
+}
