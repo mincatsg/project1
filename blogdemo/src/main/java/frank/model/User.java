cd45b8dab5305676a7a30557bf9a910eaee64098
mincatsg <1374723981@qq.com>
2020-05-15 13:12:28 +0800

    model

---
+++ blogdemo/src/main/java/frank/model/User.java
@@ -0,0 +1,20 @@
+package frank.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class User {
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String nickname;
+
+    private String avatar;
+
+
+}
