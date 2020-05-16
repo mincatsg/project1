6b2c48e3ac286e025ad80ef49a2c3abd6bdc9161
mincatsg <1374723981@qq.com>
2020-05-16 19:00:20 +0800

    service

---
+++ blogdemo/src/main/java/frank/service/CategoryService.java
@@ -0,0 +1,28 @@
+package frank.service;
+
+import frank.mapper.CategoryMapper;
+import frank.model.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+
+    @Autowired
+    private CategoryMapper categoryMapper;
+
+    public List<Category> queryCategoriesByUserId(Long id) {
+        return categoryMapper.queryCategoriesByUserId(id);
+    }
+
+    public int insert(Category category) {
+        return categoryMapper.insert(category);
+    }
+
+    public Category queryCategoryById(Long id) {
+        return categoryMapper.selectByPrimaryKey(id);
+    }
+}
