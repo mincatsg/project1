86bfbef1f8e47c074201967098b9cb3e8da15955
mincatsg <1374723981@qq.com>
2020-05-16 19:02:07 +0800

    service

---
+++ blogdemo/src/main/java/frank/service/UserService.java
@@ -0,0 +1,19 @@
+package frank.service;
+
+import frank.controller.UserController;
+import frank.mapper.UserMapper;
+import frank.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+
+    public User login(String username, String password) {
+        return userMapper.login(username, password);
+    }
+}
