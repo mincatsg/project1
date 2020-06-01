cd531762b9a5ac4da8f53e5061e7f6a96bcb5da2
mincatsg <1374723981@qq.com>
2020-06-01 11:38:55 +0800

    http

---
+++ CompanyTest/src/main/java/http/HttpServer.java
@@ -0,0 +1,25 @@
+package http;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class HttpServer {
+
+    private static final int PORT = 80;
+
+    private static final ExecutorService POOL = Executors.newCachedThreadPool();
+
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket serverSocket = new ServerSocket(PORT);
+
+        while(true){
+            //阻塞等待获取到的新的客户端连接
+            Socket socket = serverSocket.accept();
+            POOL.execute(new HttpTask(socket));
+        }
+    }
+}
